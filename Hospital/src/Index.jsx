35e8be83135fecdb1d1a5becb9cab714c3d6ff2e
Praveen <pravenravi@gmail.com>
2026-06-23 13:45:05 +0530

    Initial commit

---
+++ Hospital/src/Index.jsx
@@ -0,0 +1,10 @@
+import hero from './assets/hero.png'
+function Index(){
+    return(
+        <>
+        <h1>I am NirmalKumar</h1>
+        <img src={hero} alt="" />
+        </>
+    );
+}
+export default Index;
